fc653a6c76ae634b0730c39d3912fbf2e4c93381
ranjit1997 <44993852+ranjit1997@users.noreply.github.com>
2019-12-29 11:18:02 +0530

    3rd commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ Future<Post> get fetchPost async {
  if (response.statusCode == 200) {
     
    final responseJson = json.decode(response.body);
-   print(responseJson.body);
+   print(responseJson.data);
 
   } else {
     
